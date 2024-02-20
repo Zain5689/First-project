5d7dc43c4ff02833ca0b4e0d14a3f3657ffd18da
soha100 <sohaahmed718@gmail.com>
2024-02-20 17:21:15 +0200

    second commit

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ let menuItems = document.querySelector(".header-links");
 toggleMenu.addEventListener("click", () => {
   menuItems.classList.toggle("show-menu");
 });
+
+
+// اخبار الخليج العربى
+document.querySelector('.right').addEventListener('click', function() {
+  document.querySelector('.two').scrollLeft -= 150;
+});
+
+document.querySelector('.left').addEventListener('click', function() {
+  document.querySelector('.two').scrollLeft += 150;
+});
